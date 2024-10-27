a7849262eb794d863e730159571e5b0c1ac73839
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-10-26 19:26:28 -0500

    refactor: remove unnecessary button from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/app/ui";
-
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center gap-4">
       <h1>Home Page</h1>
-      <Button>Button</Button>
     </div>
   );
 };
