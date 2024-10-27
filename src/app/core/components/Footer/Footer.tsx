3ec20937b3112ebcb29d298bf43adc6777c9f205
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-10-27 15:47:22 -0500

    refactor(footer): add padding

--- src/app/core/components/Footer/Footer.tsx
+++ src/app/core/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-muted-foreground place-self-center">
+    <footer className="py-4 text-muted-foreground place-self-center">
       &copy; {new Date().getFullYear()} hobbescodes
     </footer>
   );
