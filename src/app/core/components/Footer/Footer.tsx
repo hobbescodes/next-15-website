ba15481decb650466e0666f3f61b9a7906c6cf7f
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-10-27 17:58:41 -0500

    feature(footer): add social nav items

--- src/app/core/components/Footer/Footer.tsx
+++ src/app/core/components/Footer/Footer.tsx
@@ -1,6 +1,24 @@
+import { NAVIGATION_LINKS } from "@/lib/constants";
+import { Button, Link } from "@/lib/ui";
+
 const Footer = () => {
   return (
-    <footer className="py-4 text-muted-foreground place-self-center">
+    <footer className="flex flex-col items-center py-4 text-muted-foreground place-self-center">
+      <div className="flex gap-2">
+        {Object.entries(NAVIGATION_LINKS.socials).map(
+          ([key, { label, icon: Icon, href }]) => (
+            <Link key={key} href={href} isExternal>
+              <Button
+                variant="unstyled"
+                className="p-2 text-foreground-primary hover:text-primary"
+              >
+                <span className="sr-only">{label}</span>
+                {Icon && <Icon className="h-4 w-4" />}
+              </Button>
+            </Link>
+          )
+        )}
+      </div>
       &copy; {new Date().getFullYear()} hobbescodes
     </footer>
   );
