5aea54dd33e2ed6f576ce1eea9de413bd59a939c
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-10-27 19:55:41 -0500

    refactor: update configuration

---
+++ src/lib/config/app.ts
@@ -0,0 +1,12 @@
+const app = {
+  name: "hobbescodes",
+  copy: "hobbescodes",
+  description: "Develop. Ship. Repeat.",
+  socials: {
+    linkedin: "https://www.linkedin.com/in/jakobhauble/",
+    github: "https://www.github.com/hobbescodes/",
+    twitter: "https://www.x.com/hobbescodes/",
+  },
+};
+
+export default app;
